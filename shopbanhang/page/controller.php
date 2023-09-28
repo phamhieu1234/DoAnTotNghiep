05ef2db965a0ac3fb309e13bf4f6b08bb6549aef
Bugs Bunny <phamhieu1234@gmail.com>
2023-09-29 00:16:18 +0700

    28092023

--- shopbanhang/page/controller.php
+++ shopbanhang/page/controller.php
@@ -1,1 +1,77 @@
-<h1>xin chào</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  color: 	rgb(0,0,0); /* Set text color to red */
+  text-align: center;
+}
+h1{
+    font-family:"Times New Roman";
+    text-align: center;
+    font-size: 35px;
+    color:#FF0000;
+}
+h2{
+    font-family:"Times New Roman";
+    text-align: center;
+    font-size: 30px;
+}
+h3{
+    font-family:"Times New Roman";
+    font-size: 30px;
+}
+h4{
+    font-family:"Times New Roman";
+    font-size: 20px;
+
+}
+</style>
+</head>
+<body>
+<h1><b>Xin chào Qúy Khách hàng</b></h1>
+<h2> <i> <b>Tên công ty: Công ty Công Nghệ TrungHieuComputer</b></i></h2>
+<p><i>Giấy CNĐKKD và mã số doanh nghiệp số: 0102799871 đăng ký lần đầu 01/08/2020,đăng ký thay đổi lần 2: 01/08/2023  do sở Kế hoạch & Đầu tư thành phố Hà Nội cấp 
+Đại diện theo pháp luật của doanh nghiệp: Ông Phạm Trung Hiếu- Giám Đốc </i></br>
+<h3><b>1. Quá trình hình thành và phát triển</h3> 
+<h4>Công ty Công Nghệ TrungHieuComputer được thành lập ngày 01-08-2020.
+Những ngày mới thành lập thành lập, TrungHieuComputer chỉ có 4 nhân sự chủ chốt.
+<br>Sau 3 năm hoạt động, hiện nay quy mô công ty tăng lên với 15 nhân viên.
+<br>Nhờ có khả năng tài chính ổn định, tính chuyên nghiệp cao trong kinh doanh và dịch vụ, khả năng bảo hành trực tiếp và hỗ trợ kỹ thuật ở mức tối đa, những mặt hàng
+mà Công ty tham gia phân phối và bán lẻ đều có chất lượng cao và được khách hàng tin tưởng. 
+<br>Hiện tại TrungHieuComputer đang là đại lý phân phối chính thức của các hãng: Gigabyte, Biostar, Intel, Kingmax, Kington, Dato, Segeat, Samsung, HKC, Hkvision, Huntkey, Orient, Fuhlen, Ensoho.
+TrungHieuComputer có một đội ngũ nhân viên có trình độ chuyên môn cao (hơn 70% đã tốt nghiệp Đại học, Cao đẳng, tung cấp chuyên nhinh tế, Kỹ thuật), đủ khả năng để có thể đáp ứng mọi yêu cầu dù là khắt khe nhất của khách hàng. 
+<br>Không những thế, đội ngũ nhân viên của TrungHieuComputer còn là những người đầy lòng nhiệt tình và  có thái độ niềm nở trong cung cách phục vụ khách hàng. 
+<br>Tất cả các cán bộ nhân viên trong công ty đều luôn luôn tâm niệm 1 điều là :  " Lấy chữ tín làm thước đo sự tồn tại và phát triển của Công ty" 
+</h4>
+<h3><b>2. Tầm nhìn TrungHieuComputer</h3>
+<h4>-Trở thành nhà phân phối các mặt linh kiện máy tính,thiết bị mạng hàng đầu
+<br>- Xây dựng  TrungHieuComputer trở thành một môi trường làm việc chuyên nghiệp nơi mà mọi cá nhân có thể phát huy tối đa sức sáng tạo. 
+<br>- Xây dựng  TrungHieuComputer trở thành một ngôi nhà chung thực sự cho mọi cán bộ nhân viên trong công ty bằng việc cùng nhau chia sẻ quyền lợi, trách nhiệm và nghĩa vụ một 
+cách công bằng và minh bạch nhất. 
+</h4>
+<h3><b>3. Giá trị cốt lõi  của TrungHieuComputer</h3>
+<h4>- Kỷ luật hướng vào tính chuyên nghiệp
+<br>- Hoàn thiện tổ chức hướng tới giảm chi phí không cần thiết để đưa giá tốt nhất tới khách hàng
+<br>- Khuyến khích tính năng động và sáng tạo
+<br>- Cam kết có trách nhiệm với nội bộ và cộng đồng, xã hội
+<br>- Làm việc tuân thủ theo pháp luật XHCN Việt Nam
+</h4>
+<h3><b>4. Quan điểm kinh doanh</h3>
+<h4>
+Đến với chúng tôi quý khách sẽ được hưởng những quyền lợi đặc biệt mà không phải bất cứ công ty nào cũng cung cấp được, đó là: 
+<br>- Hàng hoá đúng chất lượng: Đảm bảo hàng chính hãng, mới 100%. 
+<br>- Giá cả rất cạnh tranh
+<br>- Chế độ bảo hành tốt 
+<br>- Dịch vụ hoàn hảo: Đội ngũ nhân viên bán hàng và kỹ thuật luôn niềm nở và lịch sự. Đến với TrungHieuComputer bạn sẽ thấy mình là thượng đế đích thực. 
+ 
+<br>Với tất cả lòng nhiệt huyết của mình, toàn thể đội ngũ nhân viên TrungHieuComputer luôn tâm niệm thực hiện 02 điều sau  
+<br>Điều 1: Có khách hàng thì có TrungHieuComputer
+<br>Điều 2: Có TrungHieuComputer để phục vụ khách hàng 
+ 
+<br>Với mong muốn ngày càng phục vụ khách hàng tốt hơn, TrungHieuComputer luôn sẵn sàng tiếp nhận và cảm ơn những thông tin phản hồi, đóng góp từ phía khách hàng. 
+ 
+<br>Bằng tất cả nỗ lực của mình TrungHieuComputer không chỉ mong được phục vụ quý khách một lần mà là mãi mãi. 
+</h4>
+</body>
+</html>
